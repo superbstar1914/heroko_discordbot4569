f2f79bf9081829310d8589e6e0782dcc6cc5f665
superbstar1914 <89298390+superbstar1914@users.noreply.github.com>
2021-08-21 22:36:19 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,6 +10,7 @@ DSTLanguage=os.environ['DST']
 client = discord.Client()
 
 # 起動時呼叫
+
 @client.event
 async def on_ready():
     print('成功登入')
@@ -18,10 +19,8 @@ async def on_ready():
 @client.event
 async def on_message(message):
     # 送信者為Bot時無視
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-    rl.question('請輸入頻道ID\n', (channel) => {
+    
+    rl.question('878511420667494420\n', (channel) => {
         var input = function () {
             rl.question('', (msg) => {
                 if (msg == '-l') { return rl.close(); }
